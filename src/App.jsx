dd668945fb7251933034b2ad118b467f24d18ad8
James Perry <PjPerry28@gmail.com>
2025-03-10 22:07:17 -0700

    install MUI for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+// src/App.jsx
+import React from "react";
 
-import './App.css'
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 function App() {
-
   return (
-    <>
-  <p>welcome to Micheals art portfolio</p>
-    </>
-  )
+    <Container>
+      <Typography variant="h2" component="h1" gutterBottom>
+        Welcome to Micheals art portfolio
+      </Typography>
+    
+    </Container>
+  );
 }
 
-export default App
+export default App;
